6b5c84a0ab73ad4362c8bb2feaa0056e61a939d5
verywelloo <verywelloo@hotmail.com>
2024-11-12 09:12:06 +0700

    fix authMiddleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,12 +2,9 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-  let tempToken = req.headers.authorization;
+  const token = req.signedCookies.token;
 
-  const token = tempToken?.split(" ")[1] || "invalid";
-
-  // const token = req.signedCookies.token || tempToken.split(" ")[1];
-  if (!token || token === "invalid") {
+  if (!token) {
     throw new CustomError.UnauthenticatedError("Authentication Invalid");
   }
 
--- utils/jwt.js
+++ utils/jwt.js
@@ -14,12 +14,12 @@ const attachCookiesToResponse = ({ res, user }) => {
 
   const oneDay = 1000 * 60 * 60 * 24;
 
-  // res.cookie("token", token, {
-  //   httpOnly: true,
-  //   expires: new Date(Date.now() + oneDay),
-  //   secure: process.env.NODE_ENV === "production",
-  //   signed: true,
-  // });
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+    signed: true,
+  });
 
   return token;
 };
